5d3b52a00e9d81e9cc992e28d604254c946abf16
fcazzola <federico.cazzola@mail.polimi.it>
2020-02-16 23:12:59 +0100

    Code refactor

---
+++ commands.js
@@ -0,0 +1,259 @@
+const CoinGecko = require("coingecko-api");
+
+const CoinGeckoClient = new CoinGecko();
+
+const BOT_PREFIX = "coingecko";
+const PRICE_COMMAND = "price";
+const START_COMMAND = "start";
+const VOLUME_COMMAND = "volume";
+const MARKETCAP_COMMAND = "marketcap";
+const DEV_COMMAND = "dev";
+const ROI_COMMAND = "roi";
+
+let allCoins;
+
+async function getIds(name) {
+  let lowerName = name.toLowerCase();
+  console.log("lowerName: " + lowerName);
+  if (allCoins === undefined || allCoins.code != 200) {
+    allCoins = await CoinGeckoClient.coins.list();
+  }
+  if (allCoins.code != 200) {
+    throw "Get Ids:\t server error";
+  }
+
+  return allCoins.data
+    .filter(
+      coin =>
+        coin.id == lowerName ||
+        coin.symbol == lowerName ||
+        coin.name == lowerName
+    )
+    .map(coin => coin.id);
+}
+
+module.exports.commands = [
+  {
+    name: BOT_PREFIX + " " + START_COMMAND,
+    description: "Main menu.",
+    usage: "[!" + BOT_PREFIX + " " + START_COMMAND + "]",
+    verb: START_COMMAND,
+    generateMessages: async inputs => [
+      Promise.resolve(
+        "Hi! I'm GeckoBot! You can ask me crypto questions like: \n\n" +
+          "*Commands*\n" +
+          "Price\t       - `!cg p <symbol>`"
+      )
+    ]
+  },
+  {
+    name: BOT_PREFIX + " " + PRICE_COMMAND,
+    description: "Sends the price information about the coin.",
+    usage: "[!" + BOT_PREFIX + " " + PRICE_COMMAND + " coin_name]",
+    verb: PRICE_COMMAND,
+    generateMessages: async inputs =>
+      forEachCoin(inputs, async coinId => {
+        const response = await CoinGeckoClient.coins.fetch(coinId, {
+          localization: false,
+          tickers: false,
+          community_data: false,
+          developer_data: false,
+          sparkline: false
+        });
+        if (!response.success) throw "fetch error";
+        const coin = response.data;
+        const message =
+          "*" +
+          coin.name +
+          " | " +
+          coin.symbol.toUpperCase() +
+          "*\n\n" +
+          "EUR\t: €" +
+          coin.market_data.current_price.eur +
+          "\n" +
+          "USD\t: $" +
+          coin.market_data.current_price.usd +
+          "\n" +
+          "BTC\t: ฿" +
+          coin.market_data.current_price.btc +
+          "\n" +
+          "ETH\t: ♢" +
+          coin.market_data.current_price.eth +
+          "\n\n" +
+          "Mkt Cap\t: €" +
+          coin.market_data.market_cap.eur +
+          "\n" +
+          "Volume\t: €" +
+          coin.market_data.market_cap.eur +
+          "\n" +
+          "24hr %\t: " +
+          coin.market_data.price_change_percentage_24h +
+          "%\n" +
+          "7d %\t: " +
+          coin.market_data.price_change_percentage_7d +
+          "%\n" +
+          "30d %\t: " +
+          coin.market_data.price_change_percentage_30d +
+          "%\n" +
+          "1y %\t: " +
+          coin.market_data.price_change_percentage_1y +
+          "%\n\n";
+        return message;
+      })
+  },
+  {
+    name: BOT_PREFIX + " " + VOLUME_COMMAND,
+    description: "Coin's trading Volume",
+    usage: "[!" + BOT_PREFIX + " " + VOLUME_COMMAND + " coin_name]",
+    verb: VOLUME_COMMAND,
+    generateMessages: async inputs =>
+      forEachCoin(inputs, async coinId => {
+        const response = await CoinGeckoClient.coins.fetch(coinId, {
+          localization: false,
+          tickers: false,
+          community_data: false,
+          developer_data: false,
+          sparkline: false
+        });
+        if (!response.success) throw "fetch error";
+        const coin = response.data;
+        const message =
+          "*" +
+          coin.name +
+          " | " +
+          coin.symbol.toUpperCase() +
+          "*\n\n" +
+          "Volume\t: €" +
+          coin.market_data.total_volume.eur +
+          "\n";
+        return message;
+      })
+  },
+  {
+    name: BOT_PREFIX + " " + MARKETCAP_COMMAND,
+    description: "Coin's market cap",
+    usage: "[!" + BOT_PREFIX + " " + MARKETCAP_COMMAND + " coin_name]",
+    verb: MARKETCAP_COMMAND,
+    generateMessages: async inputs =>
+      forEachCoin(inputs, async coinId => {
+        const response = await CoinGeckoClient.coins.fetch(coinId, {
+          localization: false,
+          tickers: false,
+          community_data: false,
+          developer_data: false,
+          sparkline: false
+        });
+        if (!response.success) throw "fetch error";
+        const coin = response.data;
+        const message =
+          "*" +
+          coin.name +
+          " | " +
+          coin.symbol.toUpperCase() +
+          "*\n\n" +
+          "Market Cap\t: €" +
+          coin.market_data.market_cap.eur +
+          "\n";
+        return message;
+      })
+  },
+  {
+    name: BOT_PREFIX + " " + DEV_COMMAND,
+    description: "Coin's development stats",
+    usage: "[!" + BOT_PREFIX + " " + DEV_COMMAND + " coin_name]",
+    verb: DEV_COMMAND,
+    generateMessages: async inputs =>
+      forEachCoin(inputs, async coinId => {
+        const response = await CoinGeckoClient.coins.fetch(coinId, {
+          localization: false,
+          tickers: false,
+          market_data: false,
+          community_data: false,
+          developer_data: true,
+          sparkline: false
+        });
+        if (!response.success) throw "fetch error";
+        const coin = response.data;
+        const message =
+          "*" +
+          coin.name +
+          " | " +
+          coin.symbol.toUpperCase() +
+          "*\n\n" +
+          "Forks\t: " +
+          coin.developer_data.forks +
+          "\n" +
+          "Stars\t: " +
+          coin.developer_data.stars +
+          "\n" +
+          "Subscribers\t: " +
+          coin.developer_data.subscribers +
+          "\n" +
+          "Total Issues\t: " +
+          coin.developer_data.total_issues +
+          "\n" +
+          "Closed Issued\t: " +
+          coin.developer_data.closed_issues +
+          "\n" +
+          "PR Merged\t: " +
+          coin.developer_data.pull_requests_merged +
+          "\n" +
+          "PR Contributors\t: " +
+          coin.developer_data.pull_request_contributors +
+          "\n" +
+          "4-wk Commit\t: " +
+          coin.developer_data.commit_count_4_weeks;
+        return message;
+      })
+  },
+  {
+    name: BOT_PREFIX + " " + ROI_COMMAND,
+    description: "ICO's Return On Investment",
+    usage: "[!" + BOT_PREFIX + " " + ROI_COMMAND + " coin_name]",
+    verb: ROI_COMMAND,
+    generateMessages: async inputs =>
+      forEachCoin(inputs, async coinId => {
+        const response = await CoinGeckoClient.coins.fetch(coinId, {
+          localization: false,
+          tickers: false,
+          community_data: false,
+          developer_data: false,
+          sparkline: false
+        });
+        if (!response.success) throw "fetch error";
+        const coin = response.data;
+        const message =
+          "*" + coin.name + " | " + coin.symbol.toUpperCase() + "*\n\n";
+        if (coin.market_data.roi)
+          return (
+            message +
+            "ROI\t: " +
+            coin.market_data.roi.times.toFixed(5) +
+            "x " +
+            coin.market_data.roi.currency.toUpperCase() +
+            "\n"
+          );
+        else return message + "No ROI data for this coin";
+      })
+  }
+];
+
+async function forEachCoin(coins, createMessage) {
+  let messages = [];
+    for (const coin of coins) {
+      if(coin != ''){
+          const ids = await getIds(coin);
+          if (ids === undefined || ids.length == 0) {
+            messages.push(Promise.resolve("No coins found with: " + coin));
+          }
+          for (const id of ids) {
+            messages.push(createMessage(id));
+          }
+      }
+    }
+    if(messages.length == 0)
+        messages.push(Promise.resolve("You need to add some coin to the command sir"))
+  return messages;
+}
+
+module.exports.BOT_PREFIX = BOT_PREFIX;
--- index.js
+++ index.js
@@ -1,141 +1,80 @@
-const Bot = require('keybase-bot')
-const Gecko = require('./handlers');
+const Bot = require("keybase-bot");
+const Commands = require("./commands");
 
-const bot = new Bot()
-const BOT_PREFIX = "coingecko"
-const PRICE_COMMAND = "price"
-const START_COMMAND = "start"
-const VOLUME_COMMAND = "volume"
-const MARKETCAP_COMMAND = "marketcap"
-const DEV_COMMAND = "dev"
-const ROI_COMMAND = "roi"
+const bot = new Bot();
+const commands = Commands.commands;
 
 async function main() {
   try {
-    const username = process.env.KB_USERNAME
-    const paperkey = process.env.KB_PAPERKEY
-    await bot.init(username, paperkey)
-    const info = bot.myInfo()
-    console.log(`CoingeckoBot bot initialized with username ${info.username}.`)
+    const username = process.env.KB_USERNAME;
+    const paperkey = process.env.KB_PAPERKEY;
+    await bot.init(username, paperkey);
+    const info = bot.myInfo();
+    console.log(`CoingeckoBot bot initialized with username ${info.username}.`);
 
-    await bot.chat.clearCommands()
+    // console.log(commands)
+    await bot.chat.clearCommands();
     await bot.chat.advertiseCommands({
-      advertisements: [{
-        type: 'public',
-        commands: [{
-            name: BOT_PREFIX + ' ' + PRICE_COMMAND,
-            description: 'Sends the price information about the coin.',
-            usage: '[!' + BOT_PREFIX + ' ' + PRICE_COMMAND + ' coin_name]',
-          },
-          {
-            name: BOT_PREFIX + ' ' + START_COMMAND,
-            description: 'Main menu.',
-            usage: '[!' + BOT_PREFIX + ' ' + START_COMMAND + ']',
-          },
-          {
-            name: BOT_PREFIX + ' ' + VOLUME_COMMAND,
-            description: "Coin's trading Volume",
-            usage: '[!' + BOT_PREFIX + ' ' + VOLUME_COMMAND + ' coin_name]',
-          },
-          {
-            name: BOT_PREFIX + ' ' + MARKETCAP_COMMAND,
-            description: "Coin's market cap",
-            usage: '[!' + BOT_PREFIX + ' ' + MARKETCAP_COMMAND + ' coin_name]',
-          },
-          {
-            name: BOT_PREFIX + ' ' + DEV_COMMAND,
-            description: "Coin's development stats",
-            usage: '[!' + BOT_PREFIX + ' ' + DEV_COMMAND + ' coin_name]',
-          },
-          {
-            name: BOT_PREFIX + ' ' + ROI_COMMAND,
-            description: "ICO's Return On Investment",
-            usage: '[!' + BOT_PREFIX + ' ' + ROI_COMMAND + ' coin_name]',
-          },
-        ]
-      }],
-    })
+      advertisements: [
+        {
+          type: "public",
+          commands: commands.map(command => {
+            return {
+              name: command.name,
+              description: command.description,
+              useage: command.useage
+            };
+          })
+        }
+      ]
+    });
 
     const onMessage = async messageIn => {
-      let messages
-      if (messageIn.content.type !== 'text') {
-        return
+      let messages;
+      if (messageIn.content.type !== "text") {
+        return;
       }
 
-      const body = messageIn.content.text.body
-      const words = body.split(' ')
-      const [prefix, verb, ...symbols] = words
-      if (prefix !== '!' + BOT_PREFIX) {
-        return
+      const body = messageIn.content.text.body;
+      const words = body.split(" ");
+      const [prefix, verb, ...symbols] = words;
+      if (prefix !== "!" + Commands.BOT_PREFIX) {
+        return;
       }
-      console.log("verb: " + verb)
-      switch (verb) {
-        case START_COMMAND:
-          bot.chat.send(messageIn.channel, {
-            body: Gecko.generateStartMessage()
-          })
-          return
-        case PRICE_COMMAND:
-          messages = await Gecko.generatePriceMessages(symbols)
-          for (const message of messages) {
-            bot.chat.send(messageIn.channel, {
-              body: await message
-            })
-          }
-          return
-        case VOLUME_COMMAND:
-          messages = await Gecko.generateVolumeMessages(symbols)
-          for (const message of messages) {
-            bot.chat.send(messageIn.channel, {
-              body: await message
-            })
-          }
-          return
-        case MARKETCAP_COMMAND:
-          messages = await Gecko.generateMarketCapMessages(symbols)
-          for (const message of messages) {
-            bot.chat.send(messageIn.channel, {
-              body: await message
-            })
-          }
-          return
-        case DEV_COMMAND:
-          messages = await Gecko.generateDevMessages(symbols)
-          for (const message of messages) {
-            bot.chat.send(messageIn.channel, {
-              body: await message
-            })
-          }
-          return
-        case ROI_COMMAND:
-          messages = await Gecko.generateROIMessages(symbols)
-          for (const message of messages) {
-            bot.chat.send(messageIn.channel, {
-              body: await message
-            })
-          }
-          return
-        default:
-          bot.chat.send(messageIn.channel, {
-            body: 'Unknown command: ' + verb
-          })
-      }
-    }
+      console.log("verb: " + verb);
 
-    const onError = e => console.error(e)
-    console.log(`Listening for messages...`)
-    await bot.chat.watchAllChannelsForNewMessages(onMessage, onError)
+      for (const command of commands) {
+        if (command.verb == verb) {
+          messages = await command.generateMessages(symbols);
+          for (const message of messages) {
+            bot.chat.send(messageIn.channel, {
+              body: await message
+            });
+          }
+          return;
+        }
+      }
+
+      bot.chat.send(messageIn.channel, {
+        body: "Command not found"
+      });
+      return;
+    };
+
+    const onError = e => console.error(e);
+    console.log(`Listening for messages...`);
+    await bot.chat.watchAllChannelsForNewMessages(onMessage, onError);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
 async function shutDown() {
-  await bot.deinit()
-  process.exit()
+  await bot.deinit();
+  process.exit();
 }
 
-process.on('SIGINT', shutDown)
-process.on('SIGTERM', shutDown)
+process.on("SIGINT", shutDown);
+process.on("SIGTERM", shutDown);
 
 main();
